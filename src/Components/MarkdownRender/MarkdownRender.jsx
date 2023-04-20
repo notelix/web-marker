811a8c6a2193c0d5c2d40d536a84b5e6328f408f
lee88688 <lee88688@163.com>
2023-04-20 14:06:17 +0800

    format and remove unused iframe code

--- src/Components/MarkdownRender/MarkdownRender.jsx
+++ src/Components/MarkdownRender/MarkdownRender.jsx
@@ -296,7 +296,7 @@ class MarkdownRender extends React.Component {
             {markdown.content}
           </Content>
         </div>
-        <iframe src={`${process.env.PUBLIC_URL}/iframe.html`} />
+        <iframe src={`${process.env.PUBLIC_URL}/iframe.html`} style={{ width: '100%', height: '100vh' }} />
       </div>
     );
   }
